adf0b248e3606dfaf965232da83e1b12a89a8242
Joel Mounts <joel@JM.local>
2025-06-04 16:16:54 -0400

    footer edits

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="#experience">Expeirience</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#portfolio">Current Projects</a></li>
+        <li><a href="#graveyard">Graveyard</a></li>
         <li><a href="#testimonials">Testimonials</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
